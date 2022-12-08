b287c720cc8987e63a74d498cb1b704edb44df2d
Bergi84 <Klaus.Bergkirchner@gmail.com>
2022-12-08 22:42:04 +0100

    wip

--- Software/RP2040/Common/gm_terminal.cpp
+++ Software/RP2040/Common/gm_terminal.cpp
@@ -1,14 +1,301 @@
 #include "gm_terminal.h"
 
-void gm_terminal::init(TUart* aUart)
+void gm_terminal::init(TUart* aUart, TSequencer *aSeq)
 {
     mUart = aUart;
+    mSeq = aSeq;
 
     mUart->config(112500, UART_PARITY_NONE);
     mUart->installRxCb(rxCb, this);
+
+    mSeq->addTask(mTaskId, terminalTask, this);
+
+    for(int i = 0; i < TERMINAL_LINE_CNT; i++)
+    {
+        mLineBuf[i][0] = 0;
+    }
+
+    mAktLine = 0;
+    mCursorPos = 0;
+    mEscAkt = false;
+
+    mTxBufWInd = 0;
+    mTxBufRInd = 0;
 }
 
 void gm_terminal::rxCb(void* aArg)
+{
+    gm_terminal *pObj = (gm_terminal*) aArg; 
+
+    pObj->mSeq->queueTask(pObj->mTaskId);
+}
+
+void gm_terminal::terminalTask(void* aArg)
+{
+    gm_terminal *pObj = (gm_terminal*) aArg; 
+
+    uint8_t buf[16];
+    uint32_t readLen = pObj->mUart->rxBlock(buf, 16);
+    uint32_t readCnt = 0;
+
+    while(readLen)
+    {
+        if(pObj->mEscAkt)
+        {
+            buf[readCnt] = pObj->decodeEsc(buf[readCnt]);
+            if(buf[readCnt])
+            {
+                pObj->mEscAkt = false;
+            }
+        }
+        else
+        {
+            if(buf[readCnt] == CTRLSYM_ESC)
+            {
+                pObj->mEscAkt = true;
+                pObj->mEscPos = 0;
+                pObj->mEscdState = ESCD_ESC;
+            }
+        }
+
+        if(!pObj->mEscAkt)
+        {
+            if(pObj->mAktApp)
+            {
+                pObj->mAktApp->parse(buf[readCnt]);
+            }
+            else
+            {
+                pObj->recordChar(buf[readCnt]);
+            }
+        }
+
+        readCnt++;
+        if(readCnt == readLen)
+        {
+            readLen = pObj->mUart->rxBlock(buf, 16);
+            readCnt = 0;
+        }
+    }
+}
+
+uint8_t gm_terminal::decodeEsc(uint8_t aChar)
+{
+    mEscBuf[mEscPos] = aChar;
+    mEscPos++;
+    while(mEscPos == TERMINAL_MAX_ESC_LEN);
+
+    switch(mEscdState)
+    {
+        case ESCD_ESC:
+            switch(aChar)
+            {
+                case '[':
+                    mEscdState = ESCD_CSI;
+                    break;
+                case ']':
+                    mEscdState = ESCD_OSC;
+                    break;
+                default:
+                    mEscdState = ESCD_UNKNOWEN; 
+                    break;
+            }
+            break;
+
+        case ESCD_CSI:
+            if(0x40 >= aChar && 0x7E <= aChar)
+            {
+                switch(aChar)
+                {
+                    case 'A':
+                        return CTRLSYM_UP;
+                    case 'B':
+                        return CTRLSYM_DOWN;
+                    case 'C':
+                        return CTRLSYM_RIGHT;
+                    case 'D':
+                        return CTRLSYM_LEFT;
+                    default:
+                        return CTRLSYM_UNKNOWEN; 
+                }
+            }
+            break;
+
+        case ESCD_OSC:
+            if(aChar == '\\' && mEscBuf[mEscPos-2] == CTRLSYM_ESC)
+            {
+
+            }
+            break;
+
+        case ESCD_UNKNOWEN:
+            break;
+    }
+    return 0;
+}
+
+void gm_terminal::recordChar(uint8_t aChar)
+{
+    if(aChar > 0x1f && aChar < 0x7f)
+    {
+        mCpyLine = mAktLine;
+        // text
+        uint32_t inc = 0;
+        while(mLineBuf[mAktLine][mCursorPos + inc] != 0) 
+            inc++;
+
+        if(mCursorPos + inc == TERMINAL_LINE_LENGTH - 1)
+        {
+            if(inc > 1)
+                inc = inc - 2;
+            else
+                inc = 0;
+        }
+
+        while(inc != 0) 
+        {
+            mLineBuf[mAktLine][mCursorPos + inc + 1] = mLineBuf[mAktLine][mCursorPos + inc];
+        };
+
+        if(mCursorPos == TERMINAL_LINE_LENGTH - 1)
+        {
+            mLineBuf[mAktLine][mCursorPos-1] = aChar;
+            putChar(CTRLSYM_BS);
+            putChar(aChar);
+        }
+        else
+        {
+            mLineBuf[mAktLine][mCursorPos] = aChar;
+            putChar(aChar);
+            mCursorPos++;
+        }
+    }
+    else
+    {
+        // command codes
+        switch(aChar)
+        {
+            case CTRLSYM_BS:
+                if(mCursorPos != 0)
+                {
+                    mCpyLine = mAktLine;
+                    uint32_t inc = 0;
+                    while(mLineBuf[mAktLine][mCursorPos + inc] != 0) 
+                    {
+                        mLineBuf[mAktLine][mCursorPos + inc - 1] = mLineBuf[mAktLine][mCursorPos + inc];
+                        inc++;
+                    };
+                    mLineBuf[mAktLine][mCursorPos + inc - 1] = mLineBuf[mAktLine][mCursorPos + inc];
+                    mCursorPos--;
+                    putChar(aChar);
+                }
+                break;
+
+            case CTRLSYM_LF:
+                putChar(CTRLSYM_LF);
+                if(mLineBuf[mAktLine][0] = 0)
+                {
+                    newLine();
+                }
+                else
+                {
+                    parseLine();
+                    if(mAktLine == TERMINAL_LINE_CNT - 1)
+                        mAktLine = 0;
+                    else
+                        mAktLine++;
+                    
+                    mCpyLine = mAktLine;
+                    mCursorPos = 0;
+                    mLineBuf[mAktLine][mCursorPos] = 0;
+                }
+                break;
+
+            case CTRLSYM_DEL:
+                if(mLineBuf[mAktLine][mCursorPos] != 0)
+                {
+                    mCpyLine = mAktLine;
+                    uint32_t inc = 0;
+                    while(mLineBuf[mAktLine][mCursorPos + inc] != 0) 
+                    {
+                        mLineBuf[mAktLine][mCursorPos + inc] = mLineBuf[mAktLine][mCursorPos + inc + 1];
+                        inc++;
+                    };                    
+                }
+                break;
+
+            case CTRLSYM_UP:
+                if(mCpyLine == 0)
+                    mCpyLine = TERMINAL_LINE_CNT - 1;
+                else
+                    mCpyLine++;
+
+                clrLine();
+                while(mLineBuf[mCpyLine][mCursorPos] != 0) 
+                {
+                    mLineBuf[mAktLine][mCursorPos] = mLineBuf[mCpyLine][mCursorPos];
+                    putChar(mLineBuf[mCpyLine][mCursorPos]);
+                    mCursorPos++;
+                };  
+                mLineBuf[mAktLine][mCursorPos] = mLineBuf[mCpyLine][mCursorPos];
+                break;
+
+            case CTRLSYM_DOWN:
+                if(mCpyLine == TERMINAL_LINE_CNT - 1)
+                    mCpyLine = 0;
+                else
+                    mCpyLine--;
+
+                clrLine();
+                while(mLineBuf[mCpyLine][mCursorPos] != 0) 
+                {
+                    mLineBuf[mAktLine][mCursorPos] = mLineBuf[mCpyLine][mCursorPos];
+                    putChar(mLineBuf[mCpyLine][mCursorPos]);
+                    mCursorPos++;
+                };  
+                mLineBuf[mAktLine][mCursorPos] = mLineBuf[mCpyLine][mCursorPos];
+                break;
+
+            case CTRLSYM_LEFT:
+                if(mCursorPos != 0)
+                {
+                    mCursorPos--;
+                    putChar(CTRLSYM_LEFT);
+                }
+                break;
+
+            case CTRLSYM_RIGHT:
+                if(mLineBuf[mAktLine][mCursorPos] != 0)
+                {
+                    mCursorPos++;
+                    putChar(CTRLSYM_RIGHT);                   
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
+void gm_terminal::parseLine()
+{
+    // todo: implement
+
+    newLine();
+}
+
+void gm_terminal::clrLine()
+{
+    // todo: implement
+}
+
+void gm_terminal::putChar(uint8_t aChar)
+{
+    // todo: implement
+}
+
+void gm_terminal::newLine()
 {
     // todo: implement
 }
--- Software/RP2040/Common/gm_terminal.h
+++ Software/RP2040/Common/gm_terminal.h
@@ -2,8 +2,85 @@
 #define  GM_TERMINAL_H_
 
 #include "uart.h"
+#include "sequencer_armm0.h"
 
-class gm_terminal;
+class gm_termApp;
+
+enum ctrlSym_e {
+    // ascii c0 control symbols
+    CTRLSYM_ETX = 0x03, // abort, crtl + c
+    CTRLSYM_BS = 0x08,  // backspace
+    CTRLSYM_CR = 0x0D,  // cariage return
+    CTRLSYM_LF = 0x0A,  // line feed
+    CTRLSYM_ESC = 0x1B, // esc sequence
+    CTRLSYM_DEL = 0x7F, // delete
+    
+    // internal control symbols, c1 control symbols are not supported
+    CTRLSYM_UNKNOWEN = 0x80,
+    CTRLSYM_UP,
+    CTRLSYM_DOWN,
+    CTRLSYM_LEFT,
+    CTRLSYM_RIGHT
+};
+
+#define TERMINAL_LINE_LENGTH        81
+#define TERMINAL_LINE_CNT           5
+#define TERMINAL_MAX_ESC_LEN        16
+#define TERMINAL_TX_BUF_LEN         512
+
+class gm_terminal 
+{
+public:
+    void init(TUart* aUart, TSequencer *aSeq);
+
+private:
+    friend class gm_termApp;
+
+    TUart *mUart;
+    TSequencer *mSeq;
+
+    uint8_t mLineBuf[TERMINAL_LINE_CNT][TERMINAL_LINE_LENGTH];
+    uint32_t mAktLine;
+    uint32_t mCursorPos;
+    uint32_t mCpyLine;
+
+    bool mEscAkt;
+    uint8_t mEscBuf[TERMINAL_MAX_ESC_LEN];
+    uint32_t mEscPos;
+
+    gm_termApp* mRootApp;
+    gm_termApp* mAktApp;
+
+    typedef enum
+    {
+        ESCD_ESC,
+        ESCD_CSI,
+        ESCD_OSC,
+        ESCD_UNKNOWEN,
+    } escDecode_t;
+
+    escDecode_t mEscdState;
+    uint8_t mTaskId;
+
+    uint8_t mTxBuf[TERMINAL_TX_BUF_LEN];
+    uint32_t mTxBufWInd;
+    uint32_t mTxBufRInd;
+
+    static void rxCb(void* aArg);
+    static void txCb(void* aArg);
+
+    void recordChar(uint8_t aChar);
+    static void terminalTask(void* aArg);
+
+    uint8_t decodeEsc(uint8_t aChar);
+
+    void parseLine();
+    void clrLine();
+    void putChar(uint8_t aChar);
+    void newLine();
+    void exitApp();
+};
+
 
 class gm_termApp
 {
@@ -15,23 +92,20 @@ protected:
 private:
     friend class gm_terminal;
 
+    gm_terminal* mTerm;
     gm_termApp* mNext;
     const char* mKeyPhrase;
-};
 
-class gm_terminal 
-{
-public:
-    void init(TUart* aUart);
-
-private:
-    TUart *mUart; 
-
-    gm_termApp* mRootApp;
-
-    static void rxCb(void* aArg);
-
-
+    inline void putChar(uint8_t aChar)
+    {
+        mTerm->putChar(aChar);
+    }
+    virtual void start(uint8_t* aStartArg);
+    virtual void parse(uint8_t aChar);
+    inline void done()
+    {
+        mTerm->exitApp();
+    }
 };
 
 #endif /*  GM_TERMINAL_H_ */
