310611c9c596c6ba492f69bc0b4c005b5fc5b0c4
Bergi84 <klaus.bergkirchner@gmail.com>
2023-03-22 09:40:48 +0100

    wip

--- Software/RP2040/uniFw/uniFw.h
+++ Software/RP2040/uniFw/uniFw.h
@@ -37,3 +37,48 @@
 #define gpio_dls_uart0_tx           28
 #define gpio_dls_uart1_rx           25
 #define gpio_dls_uart1_tx           24
+
+// pump controller
+
+#define gpio_pc_uart0_tx            0
+#define gpio_pc_uart0_rx            1
+#define gpio_pc_uart1_tx            4
+#define gpio_pc_uart1_rx            5    
+#define gpio_pc_systemLed           6
+#define gpio_pc_spiEn               8
+#define gpio_pc_spiCs               9
+#define gpio_pc_spiClk              10
+#define gpio_pc_spiD                11
+#define gpio_pc_pwmP0               12
+#define gpio_pc_enP0                13
+#define gpio_pc_pwmP1               14
+#define gpio_pc_enP1                15
+#define gpio_pc_iRefP0              16
+#define gpio_pc_iRefP1              17
+#define gpio_pc_usbPresent          18
+#define gpio_pc_flowPuls1           21
+#define gpio_pc_flowPuls0           23
+#define gpio_pc_ledDim              24
+#define gpio_pc_iSens0              26
+#define gpio_pc_iSens1              27
+#define gpio_pc_leakSens            29   
+
+// sensor node
+
+#define gpio_sn_spiCs               1
+#define gpio_sn_spiSck              2
+#define gpio_sn_spiSdi              3
+#define gpio_sn_spiSdo              4
+#define gpio_sn_phExc               5
+#define gpio_sn_ecExcP              6
+#define gpio_sn_ecExcN              7
+#define gpio_sn_uart1_tx            8
+#define gpio_sn_uart1_rx            9
+#define gpio_sn_systemLed           10
+#define gpio_sn_uart0_tx            16
+#define gpio_sn_uart0_rx            17
+#define gpio_sn_usbPresent          18
+#define gpio_sn_sda                 24
+#define gpio_sn_scl                 25
+#define gpio_sn_din0                28
+#define gpio_sn_din1                29
